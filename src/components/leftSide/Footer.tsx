c9d6c58bc773fbbb5730922d604258360e63cbae
ABBAS BAYAT <abbas0bayat@gmail.com>
2024-05-13 18:54:04 +0330

    leftSide footer completed

--- src/components/leftSide/Footer.tsx
+++ src/components/leftSide/Footer.tsx
@@ -1,11 +1,22 @@
+import { motion } from "framer-motion";
+
 const Footer = () => {
   return (
     <div className="h-[10%] bg-transparent flex justify-around items-center">
-      <i></i>
-      <i></i>
-      <i></i>
+      <motion.i
+        whileHover={{scale: 1.5}}
+        className="fas fa-at text-white opacity-80 text-xl"
+      ></motion.i>
+      <motion.i
+        whileHover={{scale: 1.5, color: "#4479C5"}}
+        className="fab fa-linkedin text-white opacity-80 text-xl"
+      ></motion.i>
+      <motion.i
+        whileHover={{scale: 1.5, color: "#682F88"}}
+        className="fab fa-github text-white opacity-80 text-xl"
+      ></motion.i>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
